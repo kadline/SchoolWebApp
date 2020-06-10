aeab9b938aac52f1479e43d2ccf99ecaa1a5d743
bruno <bruno_lopes97@outlook.com>
2020-06-10 18:45:28 +0000

    alunis class done

---
+++ src/main/children/alunis.java
@@ -0,0 +1,50 @@
+package main.children;
+
+public class alunis {
+
+    private String firstName;
+    private String lastname;
+    private int schoolYear;
+    private int age;
+    private int workload = 45;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getSchoolYear() {
+        return schoolYear;
+    }
+
+    public void setSchoolYear(int schoolYear) {
+        this.schoolYear = schoolYear;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
+}
