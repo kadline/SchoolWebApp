0e92d20a242ae7042718c6b5b7550392a29d9d60
bruno <bruno_lopes97@outlook.com>
2020-06-10 19:50:05 +0000

    Springing entities and relations

--- src/main/Teachers/Teacher.java
+++ src/main/Teachers/Teacher.java
@@ -1,17 +1,32 @@
 package main.Teachers;
 
+import main.children.Alunis;
 import main.subjects.Subject;
+
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Teacher {
 
     private String firstName;
     private String lastName;
     private Subject subject;
+
+    @OneToMany
+    private Set<Alunis> alunis = new HashSet<>();
+
     private int workload = 45;
 
     public Teacher(String firstName, String lastName, Subject subject) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.subject = subject;
+    }
+
+    public void addAlunis(Alunis alunis){
+        this.alunis.add(alunis);
     }
 }
--- src/main/children/Alunis.java
+++ src/main/children/Alunis.java
@@ -1,15 +1,33 @@
 package main.children;
 
+import main.Teachers.Teacher;
 import main.subjects.Subject;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "Alunis")
 public class Alunis {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     private String firstName;
     private String lastname;
     private int schoolYear;
     private int age;
     private int workload = 45;
-    private Subject[] subjects;
+
+    @OneToMany(cascade = {CascadeType.ALL},
+            mappedBy = "Alunis",
+            fetch = FetchType.EAGER)
+    private HashSet<Teacher> teachers = new HashSet<>();
+
+    private Set<Subject> subjects = new HashSet<>();
 
     public String getFirstName() {
         return firstName;
@@ -51,9 +69,11 @@ public class Alunis {
         this.workload = workload;
     }
 
-    public void addSubject(Subject subject){
-        for(int i = 0; i<subjects.length; i++){
-            subjects[i] = subject;
-        }
+    public void add(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public void addSubject(Subject subject) {
+        subjects.add(subject);
     }
 }
