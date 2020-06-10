e37200903e58a22621d8be8467e464720cc6a9a0
bruno <bruno_lopes97@outlook.com>
2020-06-10 18:50:05 +0000

    classroom done

---
+++ src/main/rooms/classroom.java
@@ -0,0 +1,23 @@
+package main.rooms;
+
+public class classroom {
+
+    private int roomNumb;
+    private int maxAlunis = 20;
+
+    public int getRoomNumb() {
+        return roomNumb;
+    }
+
+    public void setRoomNumb(int roomNumb) {
+        this.roomNumb = roomNumb;
+    }
+
+    public int getMaxAlunis() {
+        return maxAlunis;
+    }
+
+    public void setMaxAlunis(int maxAlunis) {
+        this.maxAlunis = maxAlunis;
+    }
+}
